e3b42bf29c7b5ec1a0762987a99743f6c12cd2d5
manasa <amanasa726@gmail.com>
2025-04-21 15:12:02 +0530

    locking

--- day-5-dynamodb-locking/main.tf
+++ day-5-dynamodb-locking/main.tf
@@ -1,5 +1,8 @@
-resource "aws_instance" "name" {
+resource "aws_instance" "test" {
   ami = "ami-0f1dcc636b69a6438"
-  instance_type = "t2.micro"
+  instance_type = "t2.nano"
     
+     
   }
+  
+ 
