f7af3d93df3edf9580dbb9093b11b4b6833d7b7e
manasa <amanasa726@gmail.com>
2025-04-26 15:01:35 +0530

    user-data

--- day-10-user-date/main.tf
+++ day-10-user-date/main.tf
@@ -1,8 +1,7 @@
-resource "aws_instance" "import-ec2" {
+resource "aws_instance" "manasa" {
     ami = "ami-0f1dcc636b69a6438"
     instance_type = "t2.micro"
-    tags = {
-      Name = "ec2"
-    }
-  
+    availability_zone = "ap-south-1a"
+    user_data = file("test.sh")
+    
 }
